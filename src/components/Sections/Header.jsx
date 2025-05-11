f56971e60caf8e8b93fa269bba1ec6f547fca9f5
Esteban Álvarez <este.alvarez01@gmail.com>
2025-05-11 16:53:08 -0400

    Update Header.jsx

--- src/components/Sections/Header.jsx
+++ src/components/Sections/Header.jsx
@@ -12,7 +12,7 @@ const backgroundStyle = {
 
 export const Header = () => {
   return (
-    <header style={backgroundStyle} className="min-h-[100dvh] w-full bg-black transition-colors duration-500">
+    <header style={backgroundStyle} className="min-h-[100dvh] w-[100%]">
       <Nav title={"Mystic Taste."} />
       <div className="min-h-[90vh] flex flex-col gap-5 items-center justify-center text-center">
         <span className="animate__animated animate__fadeInUp relative text-2xl text-white before:content-[''] before:block before:w-5 before:md:w-16 before:h-[1px] before:bg-white before:absolute before:left-[-30px] before:md:left-[-70px] before:top-1/2 after:content-[''] after:block after:w-5 after:md:w-16 after:h-[1px] after:bg-white after:absolute after:right-[-30px] after:md:right-[-70px] after:top-1/2">
